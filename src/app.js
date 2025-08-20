e5f5ca510c5c2c5ff3767f85a72d0b4e88661451
niticache <niteeshvijayakumar500@gmail.com>
2025-08-20 10:52:33 +0530

    added app.js

--- src/app.js
+++ src/app.js
@@ -1,32 +1,43 @@
-// console.log("Let's rock")
-const express =require("express")
-const connectDb =require("./config/database")
-
-const User =require("./model/userModel")
-const {authMiddleware,userMiddleware} =require("./middlewares/auth")
-const  mongoose  = require("mongoose")
-const app =express()
-
-app.use(express.json())
-app.post("/signup",async(req,res)=>{
-    const user=new User(req.body)
-
-    await user.save()
-    res.send("User Account is created")
-})
-
-connectDb().then(()=>{
-console.log("DB Successfully connected")
-
-    app.listen(3000,()=>{
+    const express =require("express")
 
 
-        console.log("server is running in 3000 port")
-    }) 
-    
-}).catch(((err)=>{
-    console.log("DB connectioin failed, Re-try with hope , you can connect it")
-}))
+    const cookie =require("cookie-parser")
+    const connectDb =require("./config/database")
+const authRouter=require("./routes/authRouter")
+const profileRouter= require("./routes/profileRouter")
+    const User =require("./model/userModel")
+    const app =express()
+    app.use(express.json())
+    app.use(cookie())
+
+  app.use("/",authRouter)
+  app.use("/",profileRouter)
+  
+ 
+    app.patch("/user",async(req,res)=>{
+        try{
+            const userId=req.body.userId
+            console.log(userId,req.body)
+            await User.findByIdAndUpdate(userId,req.body)
+            res.send("Updated Successfully")
+                }
+        catch(err){
+            console.log("Something went wrong during updation",err)
+        }
+    })
+
+    connectDb().then(()=>{
+    console.log("DB Successfully connected")
+
+        app.listen(3000,()=>{
+
+
+            console.log("server is running in 3000 port")
+        }) 
+        
+    }).catch(((err)=>{
+        console.log("DB connectioin failed, Re-try with hope , you can connect it")
+    }))
 
 
 
