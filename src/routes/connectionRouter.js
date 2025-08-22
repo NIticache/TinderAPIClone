3b40c4829a8167ef96b6e2fa6b71dcda3dc89a37
niticache <niteeshvijayakumar500@gmail.com>
2025-08-22 19:38:26 +0530

    added accept api

--- src/routes/connectionRouter.js
+++ src/routes/connectionRouter.js
@@ -29,4 +29,28 @@ catch(err){
 }
 })
 
+connectionRouter.post("/connection/accept/:status/:userId",userAuthentication,async(req,res)=>{
+    try{
+
+    
+    const status=req.params.status
+    const toUserId=req.params.userId
+    console.log(status,toUserId)
+    const allowedStatus =["ignored","interested"]
+    if(!allowedStatus?.includes(status))
+    {
+        return ("status is not allowed")
+    }
+    const data=new ConnectionRequest({
+        fromUserId:req.user._id,
+        toUserId,
+        status
+    })
+await data.save()
+    res.send("Connection Request sent")
+}
+catch(err){
+    res.status(400).send("something went wrong",err.message)
+}
+})
 module.exports=connectionRouter
